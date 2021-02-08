bfd3d298fe524d4dd2a6b01ccb477ab64aa1cca7
eid-sukariah <iameidsukariah@gmail.com>
2021-02-08 17:24:44 +0200

    add files

---
+++ js/app.js
@@ -0,0 +1,62 @@
+'use strict'
+
+let userName = prompt('what is your name?')
+alert ('welcome  ' + userName)
+
+
+
+let user = prompt('do you know my name?')
+user = user.toLowerCase();
+if (user === 'yes' || user === 'y' ){
+    alert('you right');
+}
+    else {
+        user === 'no' || user === 'n'
+        alert ('oh no my name is eid!');
+    }
+        console.log (user)
+
+let myCountry = prompt(' am i live in jordan?')
+    myCountry = myCountry.toLowerCase();
+    if (myCountry === 'yes' || myCountry === 'y'){
+        alert ('that is right');
+    }
+    else{
+        myCountry === 'no' || myCountry === 'n'
+        alert ('no');
+    }
+        console.log (myCountry)
+
+let myColor = prompt(' do you know my fav color?')
+    myColor = myColor.toLowerCase();
+    if (myColor === 'yes' || myColor === 'y'){
+            alert ('good');
+    }
+    else{
+        myColor === 'no' || myColor === 'n'
+        alert ('my fav color is green');
+    }
+        console.log (myColor)
+
+let myAge = prompt(' am i older than 20?')
+        myAge = myAge.toLowerCase();
+    if (myCountry === 'yes' || myCountry === 'y'){
+            alert ('good');
+    }
+    else{
+        myAge === 'no' || myAge === 'n'
+         alert ('my age is 23');
+    }
+        console.log (myAge)
+
+
+let myFood = prompt('do you know my fav food?')
+    myFood = myFood.toLowerCase();
+    if (myFood === 'yes' || myFood === 'y' ){
+    alert('you right');
+}
+    else {
+        myFood === 'no' || myFood === 'n'
+        alert (' is shawrma!');
+    }
+        console.log (myFood)
