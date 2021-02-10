47c29457ead4af61bb12230e15fb72c8114d8940
Ala2Hamdan <ala2hamdan92@gmail.com>
2021-02-10 16:45:38 +0200

    add function by Ala2

--- js/app.js
+++ js/app.js
@@ -1,69 +1,84 @@
 'use strict'
-/*
+
 let userName = prompt('what is your name?')
 alert ('welcome  ' + userName)
 
-
-
+let score =0;
+function myName(){
 let user = prompt('do you know my name?')
 user = user.toLowerCase();
 if (user === 'yes' || user === 'y' ){
     alert('you right');
+    score++;
 }
     else {
         user === 'no' || user === 'n'
         alert ('oh no my name is eid!');
     }
-        console.log (user)
+        //console.log (user);
+}
+myName();
 
+function live(){
 let myCountry = prompt(' am i live in jordan?')
     myCountry = myCountry.toLowerCase();
     if (myCountry === 'yes' || myCountry === 'y'){
         alert ('that is right');
+        score++;
     }
     else{
         myCountry === 'no' || myCountry === 'n'
         alert ('no');
     }
         console.log (myCountry)
-
+}
+live();
+function color(){
 let myColor = prompt(' do you know my fav color?')
     myColor = myColor.toLowerCase();
     if (myColor === 'yes' || myColor === 'y'){
             alert ('good');
+            score++;
     }
     else{
         myColor === 'no' || myColor === 'n'
         alert ('my fav color is green');
     }
         console.log (myColor)
-
+}
+color();
+function age(){
 let myAge = prompt(' am i older than 20?')
         myAge = myAge.toLowerCase();
-    if (myCountry === 'yes' || myCountry === 'y'){
+    if (myAge === 'yes' || myAge === 'y'){
             alert ('good');
+            score++;
     }
     else{
         myAge === 'no' || myAge === 'n'
          alert ('my age is 23');
     }
-        console.log (myAge)
+        console.log (myAge);
+}
+age();
 
-
+function food(){
 let myFood = prompt('do you know my fav food?')
     myFood = myFood.toLowerCase();
     if (myFood === 'yes' || myFood === 'y' ){
     alert('you right');
+    score++;
 }
     else {
-        myFood === 'no' || myFood === 'n'
+         myFood === 'no' || myFood === 'n'
         alert (' is shawrma!');
     }
-        console.log (myFood)
+        console.log (myFood);
+}
+food();
 
 
-
-
+function myOld(){
    
 let myA = 24;
 let qu1 = prompt('Do you know how old I am');
@@ -88,34 +103,41 @@ let qu1 = prompt('Do you know how old I am');
     else {
         (qu1 == myA) 
         alert('right')
-        break
+        score++;
+        break;
     }
     
    
 }
-*/
+}
+myOld();
 
+
+function city(){
 let flag = false;
-let loveCity = ['irbid', 'amman', 'ma`an', 'ajlon', 'jarash' ]
-
-
-for (i=0; i<6; i++){
-    let tru = prompt('can you guss my fav city in jordan?')
+let loveCity = ['irbid', 'amman', 'ma`an', 'ajlon', 'jarash' ];
+let tru ;
+for (let i=0; i<6; i++){
+ tru = prompt('can you guss my fav city in jordan?');
     tru = tru.toLowerCase();
+    console.log(tru);
     
         for (let j = 0; j < loveCity.length; j++) {
             if (tru === loveCity[j]){
                 alert('good');
                 flag=true;
-                break
+                score++;
+                break;
             }
             
         }
 
-        if(found === true){
+        if(flag){
             break;
         }
     }
-alert (`thank you ${userName} your score is ${score}`)
+}
+city();
+alert (`thank you ${userName} your score is ${score}`);
 
     
